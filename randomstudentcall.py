57813796ac0a769b69393c083cb60ba086f64c65
JizelPrinceDsouza <jizelprince@gmail.com>
2025-04-05 23:47:43 +0530

    Can be used to call random students in a class.

---
+++ randomstudentcall.py
@@ -0,0 +1,6 @@
+import random
+n1=int(input('Enter the starting roll number: '))
+n2=int(input('Enter the ending roll number: '))
+task=input('Enter the task: ')
+student=random.randint(n1,n2)
+print('The roll number of the student ',task,'is ',str(student)) 
